5c57b6a3a76e29d6bf3f6bccfcf6808c7d40d561
Inou Ridder <inouridder@gmail.com>
2020-01-27 16:40:57 +0100

    ft: generate category model and spec

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,12 @@
 class Recipe < ApplicationRecord
-  CATEGORIES = %w[
+  BASE_CATEGORIES = %w[
     na breakfast brunch lunch dinner snacks appetisers soups salads
     sides pizza rice noodles pasta pies burgers mince sausages
     chicken turkey duck poultry pork lamb beef meat seafood stir-fry
     sauces vegetarian desserts baking drinks
   ].freeze
+
+  belongs_to :category, optional: true
 
   validates :title, :description, presence: true
   validates :rating, numericality: {
@@ -13,7 +15,7 @@ class Recipe < ApplicationRecord
     greater_than_or_equal_to: 0
   }
 
-  enum category: CATEGORIES, _scopes: false
+  enum category: Category.all.pluck(:name), _scopes: false
 
   def cooked!
     return true if cooked?
